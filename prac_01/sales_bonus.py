8c837f41cdfe000623b3cfc4a6710d8f1f6ab83d
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-01-26 19:47:08 +0800

    Using if loop to calculate the bonus under different sales range

--- prac_01/sales_bonus.py
+++ prac_01/sales_bonus.py
@@ -4,3 +4,9 @@ If sales are under $1,000, the user gets a 10% bonus.
 If sales are $1,000 or over, the bonus is 15%.
 """
 sales = float(input("Enter sales: $"))
+if sales < 1000:
+    bonus = sales * 0.1
+    print(f"Your bonus is ${bonus}")
+else:
+    bonus = sales * 0.15
+    print(f"Your bonus is ${bonus}")
