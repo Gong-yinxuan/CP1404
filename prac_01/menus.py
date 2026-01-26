999ae3a752f0c12cceea33d35928f74284cb3c10
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-01-27 00:07:08 +0800

    Classify the choice with if, elif, and else.

--- prac_01/menus.py
+++ prac_01/menus.py
@@ -1,7 +1,21 @@
 name = input("Enter name: ")
+
 menu = "(H)ello\n(G)oodbye\n(Q)uit"
+
 print(menu)
+
 choice = input(">>>").upper()
+
 while choice != "Q":
+
+    if choice == "H":
+        print(f"Hello, {name}")
+    elif choice == "G":
+        print(f"Goodbye, {name}")
+    else:
+        print("Invalid message")
+
     print(menu)
     choice = input(">>>").upper()
+
+print("Finished.")
