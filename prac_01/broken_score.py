82469d1ab1b01257d4663705519987d34a9cc2a5
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-01-26 20:34:18 +0800

    Test the program

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -9,9 +9,39 @@ score = float(input("Enter score: "))
 if score < 0 or score > 100:
     print("Invalid score")
 else:
-    if score > 90:
+    if score >= 90:
         print("Excellent")
-    elif score > 50:
+    elif score >= 50:
         print("Passable")
     else:
         print("Bad")
+
+"""
+Test:
+score = -20
+output = Invalid score
+
+score = 101
+output = Invalid score
+
+score = 0 
+output = bad
+
+score = 100
+output = Excellent
+
+score = 35
+output = bad
+
+score = 50
+output = passable
+
+score = 66
+output = passable
+
+score = 90
+output = Excellent
+
+score = 99
+output = Excellent
+"""
