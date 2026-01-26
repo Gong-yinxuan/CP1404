34caa09946693d29ffbde1b1c019a7c0a92b751e
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-01-26 23:47:25 +0800

    Add a while loop to check that item_num >0

--- prac_01/shop_calculator.py
+++ prac_01/shop_calculator.py
@@ -2,8 +2,11 @@ item_num=int(input("Enter the number of item: "))
 
 sum_price = 0
 
-for i in range(1,item_num+1):
-    price = float(input(f"Enter the price of item {i}: "))
-    sum_price += price
+while item_num <=0:
+    print("Invalid number of items!")
+    item_num=int(input("Enter the number of item: "))
+    for i in range(1,item_num+1):
+        price = float(input(f"Enter the price of item {i}: "))
+        sum_price += price
 
-print(f"Total price for {item_num} items is $ {sum_price:.2f}")
+    print(f"Total price for {item_num} items is $ {sum_price:.2f}")
