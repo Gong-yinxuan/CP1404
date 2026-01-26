94f6f68892df18dff3aabd43a3553d68e2b1825f
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-01-26 20:09:46 +0800

    Change if score < 0 into  if score < 0 or score > 100. Make sure the program to be able to proceed only if the input value is between 0 and 100

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -6,7 +6,7 @@ Broken program to determine score status
 # TODO: Fix this!
 
 score = float(input("Enter score: "))
-if score < 0:
+if score < 0 or score > 100:
     print("Invalid score")
 else:
     if score > 100:
