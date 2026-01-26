2d9cb373d04183f29ad36f2ddfa1edaf3d604a08
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-01-26 23:43:55 +0800

    Use f-string formatting to set the currency to 2 decimal places.

--- prac_01/shop_calculator.py
+++ prac_01/shop_calculator.py
@@ -1,5 +1,9 @@
 item_num=int(input("Enter the number of item: "))
+
 sum_price = 0
+
 for i in range(1,item_num+1):
     price = float(input(f"Enter the price of item {i}: "))
     sum_price += price
+
+print(f"Total price for {item_num} items is $ {sum_price:.2f}")
