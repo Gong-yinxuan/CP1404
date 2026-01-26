7f559a139ada3f5ec52e652a2c6b8cacb6761c3b
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-01-27 00:01:04 +0800

    Implement main menu using a while loop. keeps showing menu options until the user chooses to quit.

--- prac_01/menus.py
+++ prac_01/menus.py
@@ -2,3 +2,6 @@ name = input("Enter name: ")
 menu = "(H)ello\n(G)oodbye\n(Q)uit"
 print(menu)
 choice = input(">>>").upper()
+while choice != "Q":
+    print(menu)
+    choice = input(">>>").upper()
