50e30855d3a15b265b7bf75016989cbd90a60c1c
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-01-31 14:25:02 +0800

    Create a check_choice function that include a while loop and an if-elif-else statement, which used to get uer input and perform the corresponding operation base on the option. Call check_choice function in main function.

--- prac_02/temperature.py
+++ prac_02/temperature.py
@@ -9,19 +9,20 @@ def main():
     Q - Quit"""
     print(MENU)
     choice = input(">>> ").upper()
+    check_choice(choice, MENU)
 
-
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit : "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f"Result: {celsius:.2f} C")
-    else:
-        print("Invalid option")
-    print(MENU)
-    choice = input(">>> ").upper()
-print("Thank you.")
+def check_choice(choice,MENU):
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = celsius * 9.0 / 5 + 32
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit : "))
+            celsius = 5 / 9 * (fahrenheit - 32)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
