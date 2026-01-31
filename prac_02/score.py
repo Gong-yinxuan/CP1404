61bd0bed0a262eb9e1c3d95f411d9501089c5535
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-01-31 14:59:01 +0800

    Add main function

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,7 +2,9 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
-score = float(input("Enter score: "))
+def main():
+    score = float(input("Enter score: "))
+
 if score < 0 or score > 100:
     print("Invalid score")
 elif score >= 90:
