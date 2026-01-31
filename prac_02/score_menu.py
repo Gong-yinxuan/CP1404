a7ba1bef960adc9ec838b19e8c8ff157b0b67ffe
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-01-31 15:54:55 +0800

    Add Show Stars option to display stars equal to the score

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,5 +1,4 @@
 def main():
-
     menu = """Menu:
     (G)et a valid score
     (P)rint result
@@ -7,6 +6,7 @@ def main():
     (Q)uit"""
 
     choice = ""
+    score = None
 
     while choice != "Q":
         print(menu)
@@ -15,6 +15,19 @@ def main():
         if choice == "G":
             score = get_valid_score()
 
+        elif choice == "P":
+
+            if score is not None:
+                result = check_score(score)
+                print(f"Score {score} is {result}")
+            else:
+                print("No score entered yet. Please get a valid score first.")
+
+        elif choice == "S":
+            if score is not None:
+                show_stars(score)
+            else:
+                print("No score entered yet. Please get a valid score")
 
 
 def get_valid_score():
@@ -25,3 +38,18 @@ def get_valid_score():
         print("Score must be between 0 and 100.")
 
     return score
+
+def check_score(score):
+    if score < 0 or score > 100:
+        print("Score must be between 0 and 100.")
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+def show_stars(score):
+        stars = "*" * int(score)
+        print(stars)
+
