ee7f9c94695c5dba669863149474fd1ce3a6a3f4
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-01-31 15:37:05 +0800

    Add get_valid_score function to obtain valid score from user

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,4 +1,5 @@
 def main():
+
     menu = """Menu:
     (G)et a valid score
     (P)rint result
@@ -6,7 +7,21 @@ def main():
     (Q)uit"""
 
     choice = ""
+
     while choice != "Q":
         print(menu)
         choice = input(">>> ").upper()
 
+        if choice == "G":
+            score = get_valid_score()
+
+
+
+def get_valid_score():
+    score = float(input("Enter a score(0-100): "))
+
+    while score < 0 or score > 100:
+        score = float(input("Enter a score(0-100): "))
+        print("Score must be between 0 and 100.")
+
+    return score
