9d22078ad90872ecbe9b040376dcb0a68c950739
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-01-31 15:04:26 +0800

    Add random score generation and print result.

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,6 +2,7 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+import random
 def main():
     score = float(input("Enter score: "))
     result = check_score(score)
@@ -9,6 +10,11 @@ def main():
 
     if result == "Excellent":
         print("Excellent")
+
+    random_score = random.randint(0, 100)
+    random_result = check_score(random_score)
+    print(f"Random: {random_score} = {random_result}")
+
 
 def check_score(score):
     if score < 0 or score > 100:
