71dcefda14b387fc1839cc9f79eec78a5df826bb
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-02-03 17:05:32 +0800

    Write code to read numbers.txt files, and get the sum of the first two numbers.

--- prac_03/files.py
+++ prac_03/files.py
@@ -9,3 +9,13 @@ in_file = open("name.txt", "r")
 content = in_file.read()
 in_file.close()
 print(f"Hi {content}!")
+
+# 3.
+with open("numbers.txt", "r") as in_file:
+    first_line = in_file.readline()
+    second_line = in_file.readline()
+
+num1 = int(first_line)
+num2 = int(second_line)
+sum = num1 + num2
+print(sum)
