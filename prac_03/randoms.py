6efb2058710e22915844a763cad3960cced62e6e
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-02-03 14:24:56 +0800

    Write code to produce a random number between 1 and 100 inclusive.

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+"""
+1. I found in line 1 it uses random.randint to print numbers.
+   The smallest number is: 5
+   The largest number is: 20
+
+2. I found in line 2 it uses random.randrange to print numbers.
+   The smallest number is: 3
+   The largest number is: 9
+   It impossible to produce 4 in line 2. Line 2 only can produce 4 numbers which are 3,5,7,and 9.
+
+3. I found in line 3 it uses random.uniform to print numbers.
+   The smallest number is: 2.7610750914749227
+   The largest number is: 5.349050741343797
+"""
+import random
+print(random.randint(1,100))
