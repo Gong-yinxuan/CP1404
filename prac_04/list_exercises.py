993e56a274593341cb16d17208370dca58490d22
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-02-11 12:11:25 +0800

    Add print statements to show first, last, min, max, and average of numbers.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -4,3 +4,9 @@ for i in range(0,5):
     number = int(input(f"enter number {i+1}:"))
     print(f"Number: {number}")
     numbers.append(number)
+
+print(f"The first number is: {numbers[0]}")
+print(f"The last number is: {numbers[-1]}")
+print(f"The smallest number is: {min(numbers)}")
+print(f"The largest number is: {max(numbers)}")
+print(f"The average number is: {sum(numbers)/len(numbers)}")
