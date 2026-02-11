87110e4536aed5c1960ca713e06ac44b28b7e695
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-02-11 11:50:43 +0800

    Add a new function to display all the subject details.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -10,6 +10,7 @@ def main():
     """Program to load and display subject data from file."""
     data = load_data(FILENAME)
     print(data)
+    print_data(data)
 
 
 def load_data(filename=FILENAME):
@@ -26,5 +27,8 @@ def load_data(filename=FILENAME):
     input_file.close()
     return all_data
 
+def print_data(all_data):
+    for data in all_data:
+        print(f"{data[0]} is taught by {data[1]:12} and has {data[2]:3} students.")
 
 main()
