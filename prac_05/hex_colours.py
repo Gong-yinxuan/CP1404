3a50ea0b7ab68d5cfa99a5da49b4569ca6ebfc9b
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-03-15 15:24:44 +0800

    Add hex colours information.

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,11 @@
+HEX_COLOURS = {"Absolute Zero": "#0048ba",
+               "Acid Green": "#b0bf1a",
+               "AliceBlue": "#f0f8ff",
+               "Alizarin crimson": "#e32636",
+               "Amaranth": "#e52b50",
+               "Amber": "#e74c3c",
+               "Amethyst": "#ffbf00",
+               "AntiqueWhite": "#9966cc",
+               "AntiqueWhite2": "#66c2a5",
+               "AntiqueWhite3": "#66c2a5",}
+
