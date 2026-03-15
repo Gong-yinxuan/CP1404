019f3a7b0f28a6d82c5ed95f0558954237a2356c
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-03-15 16:57:22 +0800

    Add extract_name() function.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,6 +1,7 @@
 def main():
     email_to_name = {}
 
+    email = input("Email: ")
     while email.strip() != '':
         name = extract_name(email)
 
@@ -15,6 +16,10 @@ def main():
         print(f"{name} ({email})")
 
 def extract_name(email):
+    name_parts = email.split("@")[0]
+    parts = name_parts.split(".")
+    name = " ".join(parts).title()  # 用空格联系两个部分，首字母大写
+    return name
 
 main()
 
