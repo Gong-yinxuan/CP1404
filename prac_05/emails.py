974f1e414a3aa452a2b04e79dab182fd1d09c941
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-03-15 16:55:47 +0800

    Add loop to prompt user for email until blank input.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,6 +1,20 @@
 def main():
     email_to_name = {}
 
+    while email.strip() != '':
+        name = extract_name(email)
+
+        confirm = input(f"Is your name {name}? (Y/n): ")
+        if confirm.lower() == "n" or confirm.lower() == "no":
+            name = input("Name: ")
+
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+def extract_name(email):
 
 main()
 
