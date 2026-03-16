6294e5348581ab9527fd78a6fb94e5a238a542d8
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-03-16 12:03:07 +0800

    Add champion countries collection.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -9,6 +9,7 @@ with open(FILENAME, "r", encoding="utf-8") as in_file:
     for row in reader:
         champion = row["Champion"]
         country = row["Country"]
+        countries.add(country)
 
         if champion not in data:
             data[champion] = 1
