29cea5eb7461fa5694b7f1fec5eea3be1c06e77d
xiaoqingning <yinxuan.gong@my.jcu.edu.au>
2026-03-16 12:05:19 +0800

    Count the total number of champions.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -6,9 +6,11 @@ data = {}
 with open(FILENAME, "r", encoding="utf-8") as in_file:
     reader = csv.DictReader(in_file)
     countries = set()
+    champion_count = 0
     for row in reader:
         champion = row["Champion"]
         country = row["Country"]
+        champion_count += 1
         countries.add(country)
 
         if champion not in data:
